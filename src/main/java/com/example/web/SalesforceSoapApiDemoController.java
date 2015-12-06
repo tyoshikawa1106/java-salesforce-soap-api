0a6444e3d1c893ff0f9e3cd7705137abe39d49fb
Taiki <tyoshikawa1106@gmail.com>
2015-12-06 15:42:22 +0900

    commit

--- src/main/java/com/example/web/SalesforceSoapApiDemoController.java
+++ src/main/java/com/example/web/SalesforceSoapApiDemoController.java
@@ -4,8 +4,13 @@ import com.example.app.SalesforceApiUtil;
 import com.example.object.Account;
 import com.sforce.async.AsyncApiException;
 import com.sforce.soap.partner.PartnerConnection;
+import com.sforce.soap.partner.QueryResult;
+import com.sforce.soap.partner.sobject.SObject;
 import com.sforce.ws.ConnectionException;
 import com.sforce.ws.ConnectorConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
@@ -24,7 +29,6 @@ public class SalesforceSoapApiDemoController {
 	
 	@ModelAttribute("loginUser")
 	LoginUser loginUser() {
-        System.out.println("create loginUser");
         return new LoginUser();
     }
 	
@@ -36,7 +40,6 @@ public class SalesforceSoapApiDemoController {
     @RequestMapping(value="/home", method=RequestMethod.GET)
     public String showHome(LoginUser loginUser, Model model) throws ConnectionException {
         PartnerConnection partnerConnection = loginUser.getPartnerConnection();
-        System.out.println("UserInfo = " + partnerConnection.getUserInfo());
         
         UserInfo userInfo = new UserInfo();
         userInfo.setUserFullName(partnerConnection.getUserInfo().getUserFullName());
@@ -48,11 +51,37 @@ public class SalesforceSoapApiDemoController {
     @RequestMapping(value="/account", method=RequestMethod.GET)
     public String showAccount(LoginUser loginUser, Model model) throws ConnectionException {
         PartnerConnection partnerConnection = loginUser.getPartnerConnection();
-        System.out.println("UserInfo = " + partnerConnection.getUserInfo());
+        
+        String soqlQuery = "SELECT Name, AccountNumber FROM Account LIMIT 200";
+        QueryResult qr = partnerConnection.query(soqlQuery);
+        
+        List<Account> accounts = new ArrayList<Account>();
+        
+        boolean done = false;
+        int loopCount = 0;
+        // Loop through the batches of returned results
+        while (!done) {
+          System.out.println("Records in results set " + loopCount++ + " - ");
+          SObject[] records = qr.getRecords();
+          // Process the query results
+          for (int i = 0; i < records.length; i++) {
+        	Account acc = new Account();
+        	acc.setName(String.valueOf(records[i].getField("Name")));
+        	acc.setAccountNumber(String.valueOf(records[i].getField("AccountNumber")));
+        	// Add List
+        	accounts.add(acc);
+          }
+          if (qr.isDone()) {
+            done = true;
+          } else {
+            qr = partnerConnection.queryMore(qr.getQueryLocator());
+          }
+        }
         
         Account account = new Account();
         account.setName("Salesforce.com");
         model.addAttribute("account", account);
+        model.addAttribute("accounts", accounts);
 
         return "account";
     }
